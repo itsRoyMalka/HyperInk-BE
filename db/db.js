bd573bdf4123859a6fc3627b471ade1ce291f4af
itsRoyMalka <115025973+itsRoyMalka@users.noreply.github.com>
2023-08-16 18:06:08 +0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -5,10 +5,7 @@ export const dbConnect = async () =>{
     try{
 
        
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        await mongoose.connect(process.env.MONGO_URI)
 
 
     }catch(error){
